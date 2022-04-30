7501c99b76e8d9d6219ad4da005c630f18795ef4
darkmage <165251+mikedesu@users.noreply.github.com>
2022-04-30 15:11:58 -0500

    first

---
+++ README.md
@@ -0,0 +1,90 @@
+```
+                   _ _      __              __   
+  ____ ___________(_|_)____/ /_  ____ _____/ /__ 
+ / __ `/ ___/ ___/ / / ___/ __ \/ __ `/ __  / _ \
+/ /_/ (__  ) /__/ / (__  ) / / / /_/ / /_/ /  __/
+\__,_/____/\___/_/_/____/_/ /_/\__,_/\__,_/\___/ 
+
+                   -by darkmage 
+               www.evildojo.com
+```
+
+*doin' ascii? throw some shade on it*
+*what is an "ascii bird" even? idk*
+**i know what shade is**
+
+1. shade keeps me cool on a hot day
+2. shade is what haters throw at me so imma throw back at them
+3. shade is the spirit of shadow in the old SNES RPG: **Secret of Mana**
+
+Now, shade is an ASCII Art Editor.
+
+-----
+
+### What is shade?
+
+Shade is an upcoming ASCII creation and editing tool.
+It is written from the ground up in Python using the curses library.
+
+### Why write shade?
+
+Highly dissatisfied with the complete lack of options in the ascii-creation department, I decided to begin implementing a pure CLI tool. I began writing shade late at night on April 30th, 2022. It was easier than expected to get something close to what I wanted right-away. Now is just about fine-tuning the system.
+
+The state of developers online is largely sided towards web applications. I admire the simplicity and purity of CLI and 'tui' tools and programs. They can often perform much better than their web counterparts. 
+
+### Who cares about ASCII art in 2022+?
+
+A good number of people care about ASCII art.
+
+- Blocktronics: [http://blocktronics.org/](http://blocktronics.org/)
+- AciD Productions: [https://16colo.rs/group/acid](https://16colo.rs/group/acid)
+
+### Web ASCII editors
+
+- ASCII Blaster: [https://asdf.us/asciiblaster/](https://asdf.us/asciiblaster/)
+- asciibird: [https://asciibird.jewbird.live/](https://asciibird.jewbird.live/)
+
+### Using shade
+
+**Under construction**
+
+'c': go back one color pair
+'v': go forward one color pair
+'q': quit
+arrow keys: cursor navigation
+tilde: switch mode between normal and text modes
+space bar: 
+    - normal mode: place a block
+    - text mode: place a space character
+
+### Features On The Way
+
+- Saving your current work
+- Exporting to:
+    - irc
+    - ascii
+    - png
+    - gif
+- Upload to:
+    - termbin
+- Half-block support
+- Copy / paste
+- Multiple tabs
+- Easy switching of foreground / background colors
+- More!
+
+-----
+
+Did you find this tool helpful, useful, interesting, or cool? Maybe none of those? Maybe you thought it sucked and I need more encouragement.
+
+Either way, please consider sending me crypto through one of these addresses:
+
+Bitcoin: 38p6p4gLqfvZ4LShmWpNnphN1TjB8WgT8M
+Litecoin: MU8YSVUVwKDiaQfPurW8cVzxjbeZc2qv2S
+Ethereum: 0xC8E4ac8ee54698dDf47d7343e12aB83985F3900A
+Monero: 87eZsjdCFHtQXEf4A2Mer1XjJmLmD6N1b2maovAR4XtSYs4EupxZB7UfmG9eyb8qXpVvR9HdsEpf7K3A762w3LcNTg1fLTC 
+
+Or, you can become a patron on my [Patreon page](https://www.patreon.com/darkmage)!
+
+I stream regularly on Twitch as well at: [https://www.twitch.tv/darkmage666](https://www.twitch.tv/darkmage666)
+
---
+++ main.py
@@ -0,0 +1,144 @@
+from curses import wrapper
+from curses import start_color
+from curses import use_default_colors
+from curses import init_pair
+from curses import color_pair
+import curses
+
+x = 0
+y = 0
+current_color_pair = 1
+max_color_pair_defined = -1
+mode = 'normal'
+max_x = -1
+max_y = -1
+current_character = -1
+hud_color = 8
+quit = False
+movement_keys = ['KEY_LEFT', 'KEY_RIGHT', 'KEY_UP', 'KEY_DOWN']
+
+def switch_mode():
+    global mode
+    if mode == 'normal':
+        mode = 'text'
+    elif mode == 'text':
+        mode = 'normal'
+
+def add_character(s, c):
+    global x
+    s.addstr(y, x, c, color_pair(current_color_pair))
+    #s.addstr(y, x, '▀', color_pair(current_color_pair))
+    x += 1
+
+def reset_cursor(s, dy=0, dx=0):
+    global y, x
+    y = dy
+    x = dx
+    s.move(y, x)
+
+def draw_hud(s):
+    for j in range(max_y-2, max_y):
+        for i in range(0, max_x):
+            try:
+                s.addstr(j, i, ' ', color_pair(hud_color))
+            except:
+                pass
+    s.addstr(max_y-2, 0, f'y: {y:3} | ', color_pair(hud_color)) 
+    s.addstr('#', color_pair(current_color_pair))
+    s.addstr(f'{current_color_pair:3}', color_pair(hud_color))
+    s.addstr(max_y-1, 0, f'x: {x:3} |', color_pair(hud_color)) 
+    reset_cursor(s, y, x)
+
+def handle_input(s, c):
+    global y, x
+    global mode
+    global current_color_pair
+    global current_character
+    global hud_color
+    global quit
+    if not c in movement_keys:
+        if mode == 'normal':
+            if c == '`':
+                switch_mode()
+            elif c == ' ':
+                s.addstr(y, x, c, color_pair(current_color_pair))
+                x += 1
+            elif c == 'c':
+                current_color_pair -= 1
+                if current_color_pair < 0:
+                    current_color_pair = max_color_pair_defined-1
+            elif c == 'v':
+                current_color_pair += 1
+                if current_color_pair >= max_color_pair_defined:
+                    current_color_pair = 0
+            elif c == 'q':
+                quit = True
+            elif c == 'i': #info
+                attr = s.inch(y, x)
+                char = chr(attr & curses.A_CHARTEXT)
+                color = attr & curses.A_COLOR
+                current_character = char
+                my_attrs = f'{bin(attr)} {hex(attr)} {attr} {char} {bin(color)} {hex(color)} {color}'
+                s.addstr(y, x+1, str(my_attrs))
+        elif mode == 'text':
+            if c == '`':
+                switch_mode()
+            else:
+                add_character(s, c)
+    elif c == movement_keys[0]:
+        x -= 1
+    elif c == movement_keys[1]:
+        x += 1
+    elif c == movement_keys[2]:
+        y -= 1
+    elif c == movement_keys[3]:
+        y += 1
+    check_cursor_bounds()
+    s.move(y, x)
+
+def check_cursor_bounds():
+    global y, x
+    if x <= 0:
+        x = 0
+    elif x > max_x - 2:
+        x = max_x - 2
+    if y <= 0:
+        y = 0
+    elif y > max_y - 3:
+        y = max_y - 3
+
+def define_color_pairs():
+    global max_color_pair_defined
+    [init_pair(i, 255, i) for i in range(256)]
+    max_color_pair_defined = 256
+
+def draw_initial_ascii(s):
+    lines = [ 'Welcome to shade v0.1',
+        'by darkmage',
+        'www.evildojo.com'
+    ]
+    y0 = 0
+    for i in lines:
+        s.addstr(y0, 0, i, color_pair(8))
+        y0 += 1
+
+def main(s):
+    global mode
+    global y, x
+    global max_y, max_x
+    s.clear()
+    max_y, max_x = s.getmaxyx()
+    start_color()
+    use_default_colors()
+    define_color_pairs()
+    draw_initial_ascii(s)
+    y, x = s.getyx()
+    s.refresh()
+    while quit != True:
+        draw_hud(s)
+        c = s.getkey()
+        handle_input(s, c)
+        s.refresh()
+
+if __name__ == '__main__':
+    wrapper(main)
